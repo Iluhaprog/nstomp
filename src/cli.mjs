b2f2e1091cca881aa7fb60cbde555a834ea25b9a
Ilya Novak <rickmortyand4@gmail.com>
2022-01-31 05:44:42 +0300

    fix: Changed version.

--- src/cli.mjs
+++ src/cli.mjs
@@ -8,7 +8,7 @@ export function run(argv) {
 
 	const description = getDescription();
 
-	program.version("1.0.0", "-v, --version", "output the current version");
+	program.version("1.0.1", "-v, --version", "output the current version");
 	program.description(description.main);
 	program
 		.option("-u, --url <url...>", description.url)
